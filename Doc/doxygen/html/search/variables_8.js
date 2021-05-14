38deff997cf0515d78d731637f72acaa6445b045
Clément LE STRAT <clestrat75@gmail.com>
2021-05-14 10:28:44 +0200

    Création Doc/doxygen/Doxyfile pour générer la documentation Suppression des docs Doxygen sur github Correction .gitignore

--- Doc/doxygen/html/search/variables_8.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['y_155',['y',['../structcubies.html#a0a2f84ed7838f07779ae24c5a9086d33',1,'cubies']]]
-];
---
+++ Doc/doxygen/output/html/search/searchdata.js
@@ -0,0 +1,39 @@
+var indexSectionsWithContent =
+{
+  0: "abcdeglmnorstuwxy",
+  1: "cnr",
+  2: "dmr",
+  3: "acdmrs",
+  4: "bcnostxy",
+  5: "t",
+  6: "bcdegloruwy",
+  7: "mrst",
+  8: "r"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "enums",
+  6: "enumvalues",
+  7: "defines",
+  8: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "Tout",
+  1: "Structures de données",
+  2: "Fichiers",
+  3: "Fonctions",
+  4: "Variables",
+  5: "Énumérations",
+  6: "Valeurs énumérées",
+  7: "Macros",
+  8: "Pages"
+};
+
