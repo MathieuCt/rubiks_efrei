5caa25d826c71fa366eca318eee61ae9f400e996
Nagisa19 <clestrat75@gmail.com>
2021-05-20 20:06:21 +0200

    Modification du ficher du menu

--- Menu.c
+++ Menu.c
@@ -7,37 +7,93 @@
 void show_menu(rubiks_side *rubiks)
 {
     draw_rubiks(rubiks);
-    int choice = 0, choice2 = TRUE;
+    int start_choice, end_choice = 1, move_choice, side_choice;
 
-    printf("Fates votre choix (attention vous ne pouvez rezntrez que 1 ou 2 comme choix).\n");
-    printf("Mélanger le rubiks = 1.\n");
-    printf("Résoudre le rubiks = 2.\n");
-    while (choice != 1 && choice != 2)
+    while (end_choice)
     {
+        printf("Fates votre choix (attention vous ne pouvez rezntrez que 1 ou 2 comme choix).\n");
+        printf("1->Mélanger le rubiks.\n");
+        printf("2->Résoudre le rubiks.\n");
+        printf("3->Faire vos propre mouvements.\n");
         printf("Rentrez votre choix: ");
-        scanf("%d", &choice);
-    }
+        scanf("%d", &start_choice);
+        while (start_choice != 1 && start_choice != 2 && start_choice != 3)
+        {
+            printf("Rentrez votre choix: ");
+            scanf("%d", &start_choice);
+        }
 
-    if (choice == 1)
-    {
-        mix_rubiks(rubiks);
-        draw_rubiks(rubiks);
-    }
-    else
-    {
-        solve_rubiks(rubiks);
-        draw_rubiks(rubiks);
-    }
+        printf("--------------------------------\n");
 
-    printf("Voulez vous continuer? (1/0): ");
-    scanf("%d", &choice2);
+        if (start_choice == 1)
+        {
+            mix_rubiks(rubiks);
+            draw_rubiks(rubiks);
+        }
+        else if (start_choice == 2)
+        {
+            solve_rubiks(rubiks);
+            draw_rubiks(rubiks);
+        }
+        else
+        {
+            printf("1->'move_side_clockwise'.\n");
+            printf("2->'move_side_anticlockwise'.\n");
+            printf("Rentrez votre choix: ");
+            scanf("%d", &move_choice);
+            while (move_choice != 1 && move_choice != 2)
+            {
+                printf("Rentrez votre choix: ");
+                scanf("%d", &move_choice);
+            }
 
-    if (choice2 == 1)
-    {
-        show_menu(rubiks);
-    }
-    else
-    {
-        exit(0);
+            printf("--------------------------------\n");
+
+            if (move_choice == 1)
+            {
+                printf("1->blanc.\n");
+                printf("2->orange.\n");
+                printf("3->vert.\n");
+                printf("4->rouge.\n");
+                printf("5->bleu.\n");
+                printf("6->jaune.\n");
+                printf("Rentrez votre choix: ");
+                scanf("%d", &side_choice);
+                while (side_choice != 1 && side_choice != 2 && side_choice != 3 && side_choice != 4 &&
+                       side_choice != 5 && side_choice != 6)
+                {
+                    printf("Rentrez votre choix: ");
+                    scanf("%d", &side_choice);
+                }
+
+                move_side_clockwise(rubiks, side_choice - 1);
+                draw_rubiks(rubiks);
+                printf("--------------------------------\n");
+            }
+            else
+            {
+                printf("1->blanc.\n");
+                printf("2->orange.\n");
+                printf("3->vert.\n");
+                printf("4->rouge.\n");
+                printf("5->bleu.\n");
+                printf("6->jaune.\n");
+                printf("Rentrez votre choix: ");
+                scanf("%d", &side_choice);
+                while (side_choice != 1 && side_choice != 2 && side_choice != 3 && side_choice != 4 &&
+                       side_choice != 5 && side_choice != 6)
+                {
+                    printf("Rentrez votre choix: ");
+                    scanf("%d", &side_choice);
+                }
+
+                move_side_anticlockwise(rubiks, side_choice - 1);
+                draw_rubiks(rubiks);
+                printf("--------------------------------\n");
+            }
+        }
+
+        printf("Voulez vous continuer? (1/0): ");
+        scanf("%d", &end_choice);
     }
 }
