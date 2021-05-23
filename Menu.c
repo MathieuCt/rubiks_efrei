bf6807d95bfc50b7c4b1b3ac7f55740698d53ae7
Clément LE STRAT <clestrat75@gmail.com>
2021-05-23 19:09:22 +0200

    Commentaires sur les fonctions Menus.c

--- Menu.c
+++ Menu.c
@@ -4,11 +4,19 @@
 
 #include "Menu.h"
 
+/**
+ * Cette fonction affiche le menu principal et tous les sous-menus.
+ *
+ * @param rubiks Un pointeur vers le rubik's cube actuellement en cours d'utilisation
+ */
 void show_menu(rubiks_side *rubiks)
 {
-    draw_rubiks(rubiks);
-    int start_choice, end_choice = TRUE, move_choice, side_choice;
+    int start_choice, end_choice = true, move_choice, side_choice;
 
+    // A chaque fois qu'on affiche le menu, on commence par ré-afficher l'état actuel du cube.
+    draw_rubiks(rubiks);
+
+    // Gestion des choix du menu principal
     while (end_choice)
     {
         printf("Faites votre choix (attention vous ne pouvez rentrer que des chiffes comme choix).\n");
@@ -22,7 +30,7 @@ void show_menu(rubiks_side *rubiks)
         start_choice = choice_menu(tab);
 
         printf("--------------------------------\n");
-
+        // Choix des différents sous-menus.
         switch (start_choice)
         {
             case 1:
@@ -36,11 +44,6 @@ void show_menu(rubiks_side *rubiks)
             case 3:
                 printf("1-> Mouvement horaire.\n");
                 printf("2-> Mouvement antihoraire.\n");
-                //printf("Rentrez votre choix: ");
-                /*do
-                {
-                    move_choice  = getchar();
-                }while (move_choice != '1' && move_choice != '2');*/
                 char tab2[2] = {1, 2};
                 move_choice = choice_menu(tab2);
 
@@ -107,29 +110,36 @@ void show_menu(rubiks_side *rubiks)
 
 /**
  * Cette fonction permet à l'utilisateur de remplir un cube
+ *
+ * @param rubiks Un pointeur vers le rubik's en cours de traitement.
  */
+
 void choose_color(rubiks_side *rubiks){
-    // liste des cubie disponibles pour êtres positioner
+    // liste des cubies disponibles pour être positionnés correctement.
     cubies liste_corner[24], liste_edge[24];
     int len_liste_corner = 0, len_liste_edge = 0;
 
-    // création d'un rubiks cube de référence (déjà résolu
+    // création d'un rubiks cube de référence (déjà résolu, et que nous modifierons en fonction des étapes de la saisie)
     rubiks_side reference_rubiks[6];
     rubiks_creation(reference_rubiks);
-    //initialisation avec la couleur grise
+
+    // On intialise tout le Rubik's avec la couleur grise, sauf les centres qui ne bougeront pas.
     for(int i = WHITE ; i <= YELLOW ; i++){
         for(int j = 0 ; j < 9 ; j++) {
             // si le cubie est un centre, il est déjà correctement positionné
             if(rubiks[i].cubie[j].type != CENTER) {
-                // on lui retire sa couleur
+                // on lui retire sa couleur, pour ne plus la traiter par la suite.
                 rubiks[i].cubie[j].color = NO_COLOR;
             }
         }
     }
+
     // on complète la liste des edge et des corners à l'aide du rubiks de référence
     len_liste_corner = creation_liste_cubie(reference_rubiks,liste_corner, CORNER);
     len_liste_edge = creation_liste_cubie(reference_rubiks, liste_edge, EDGE);
-    //remplir le rubiks
+
+    // A partir d'ici, on va demander à l'utilisateur de  remplir le rubiks
+    // On ne propose que les états disponibles à être entrés.
     for(int i = WHITE ; i <= YELLOW ; i++){
         for(int j = 0 ; j < 9 ; j++) {
             if(rubiks[i].cubie[j].type != CENTER && rubiks[i].cubie[j].color == NO_COLOR){
@@ -155,21 +165,21 @@ void choose_color(rubiks_side *rubiks){
 }
 
 /**
- * permet de récupérer le choix de l'utilisateur
- * @param reference_rubiks pointeur sur un rubiks
- * @param rubiks pointeur sur un rubiks
- * @param type type de cubie à placer
- * @param liste des cubie disponibles
- * @param longueur de la liste
- * @param face du cubie à modifier
- * @param numéro du cubie à modifier
+ * Cette fonction permet de récupérer le choix de l'utilisateur en fonction des cubies disponibles.
+ * @param reference_rubiks Pointeur sur un rubik's de référence
+ * @param rubiks Pointeur sur le rubik's de travail
+ * @param type Type de cubie à placer
+ * @param liste_cubie Liste des cubie disponibles
+ * @param len_liste Longueur de la liste précédente
+ * @param face Face du cubie à modifier
+ * @param num_cubie Numéro du cubie à modifier
  */
 void choice_cubie(rubiks_side *reference_rubiks, rubiks_side *rubiks, T_CUBIE_TYPE type, cubies *liste_cubie, int len_liste, int face, int num_cubie)
 {
     char tabcolor[][30] = {WHT "W" RESET, ORG "O" RESET, GRN "G" RESET, ARED "R" RESET, BLU "B" RESET, YEL "Y" RESET};
     int choice;
 
-    // imprimer la liste des possibilités des cubies disponibles
+    // imprimer la liste des cubies encore disponibles
     for(int i = 0; i < len_liste ;i++){
         printf("Choix %d : %s, %s", i, tabcolor[liste_cubie[i].color],
                tabcolor[reference_rubiks[liste_cubie[i].neighbours[0].num_side].cubie[liste_cubie[i].neighbours[0].num_cubie].color]);
@@ -177,6 +187,7 @@ void choice_cubie(rubiks_side *reference_rubiks, rubiks_side *rubiks, T_CUBIE_TY
             printf(", %s",
                    tabcolor[reference_rubiks[liste_cubie[i].neighbours[1].num_side].cubie[liste_cubie[i].neighbours[1].num_cubie].color]);
         printf(" | ");
+        // On regroupe les choix de menus, par 6 pour éclairer un peu les menus.
         if(i % 6 == 0 && i != 0){
             printf("\n");
         }
@@ -209,9 +220,12 @@ void choice_cubie(rubiks_side *reference_rubiks, rubiks_side *rubiks, T_CUBIE_TY
 }
 
 /**
- *
- *
- * */
+ * Cette fonction va créer la liste des cubies disponibles à la saisie utilisateur
+ * @param rubiks Pointeur vers le rubik's de travail
+ * @param liste Liste des cubies disponibles en sortie
+ * @param type Type de cubie à travailler, soit EDGE, soit CORNER dans notre cas, puisque les centres sont placés d'avance.
+ * @return
+ */
 int creation_liste_cubie(rubiks_side * rubiks,cubies * liste, T_CUBIE_TYPE type){
 
     int len_liste = 0;
@@ -228,6 +242,9 @@ int creation_liste_cubie(rubiks_side * rubiks,cubies * liste, T_CUBIE_TYPE type)
     return len_liste;
 }
 
+/**
+ * Purge des buffers de getchar(), car elle conserve les return du clavier. On les supprime donc.
+ */
 void clear_buffer()
 {
     int c = 0;
@@ -237,7 +254,13 @@ void clear_buffer()
     }
 }
 
-int choice_menu(char * soluce)
+/**
+ * Cette fonction va laisser l'utilisateur choisir des entrées du menu parmi une liste de choix passée en paramètre.
+ *
+ * @param list_of_choices Liste de choix disponibles à la saisie
+ * @return L'entrée choisit par l'utilisateur
+ */
+int choice_menu(char * list_of_choices)
 {
     int choice = 0, good_choice = false;
 
@@ -250,18 +273,18 @@ int choice_menu(char * soluce)
         // getchar renvoie le code ascii du caractère saisie. 0 est égal à 48, 1 = 49, etc. Donc, on retranche 48 au code ascii récupéré
         choice -= 48;
 
-        for (int i = 0; i < sizeof(soluce); i++)
+        for (int i = 0; i < sizeof(list_of_choices); i++)
         {
-            if (soluce[i] == choice)
+            if (list_of_choices[i] == choice)
             {
                 good_choice = true;
             }
         }
 
-        if (!good_choice)
-        {
-            printf("\b");
-        }
+//        if (!good_choice)
+//        {
+//            printf("\b");
+//        }
     } while (!good_choice);
     return choice;
 }
